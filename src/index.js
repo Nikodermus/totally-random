cceb311f20422cda99714d7a8c0bc909c46d8341
Nicolas M. Pardo <nicolasm@nikodermus.media>
2020-10-10 15:57:23 -0500

    feat(src/index): create random number util

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-export default 'Welcome to totally-random';
+const totallyRandom = (...args) => {
+    let min = 0;
+    let max = 100;
+
+    if (args.length === 1 && typeof args[0] === 'number') {
+        [max] = args;
+    } else if (args.length === 2) {
+        [min, max] = args;
+    }
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export default totallyRandom;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,42 @@
-import expect from 'expect'
+import expect from 'expect';
 
-import message from 'src/index'
+import tr from 'src/index';
 
-describe('Module template', () => {
-  it('displays a welcome message', () => {
-    expect(message).toContain('Welcome to totally-random')
-  })
+describe('Totally Random', () => {
+    it('returns a number', () => {
+        const testDefault = new Array(10).map(() => tr());
+
+        testDefault.forEach(v => {
+            expect(v).toBeA('number');
+        });
+    });
+
+
+    it('returs by default a number between 0 and 100', () => {
+        const testDefault = new Array(10).map(() => tr());
+
+        testDefault.forEach(v => {
+            expect(v).toBeLessThanOrEqualTo(100);
+            expect(v).toBeGreaterThanOrEqualTo(0);
+        });
+    });
+
+    it('returs by default a number between 0 and 17', () => {
+        const testDefault = new Array(10).map(() => tr(17));
+
+        testDefault.forEach(v => {
+            expect(v).toBeLessThanOrEqualTo(17);
+            expect(v).toBeGreaterThanOrEqualTo(0);
+        });
+    });
+
+    it('returs by default a number between 23 and 45', () => {
+        const testDefault = new Array(10).map(() => tr(23, 45));
+
+        testDefault.forEach(v => {
+            expect(v).toBeLessThanOrEqualTo(45);
+            expect(v).toBeGreaterThanOrEqualTo(23);
+        });
+    });
 })
+
